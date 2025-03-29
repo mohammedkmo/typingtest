bb1fed288c8a6078eeb840e0a7040b7a3289eb43
mohammedkmo <alrasamking123@gmail.com>
2025-03-29 17:11:08 +0300

    fix auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -89,7 +89,7 @@ export async function sendVerificationEmail(email: string): Promise<string> {
       user: process.env.EMAIL_SERVER_USER,
       pass: process.env.EMAIL_SERVER_PASSWORD,
     },
-    secure: process.env.IS_SECURE === "true",
+    secure: true,
   });
   
   // Send email with the code
